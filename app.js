7e7e258bf61eae55d8bfb2806d850d6d1f78ee2b
Viktoria Vishniakova <vikaveter1563@yandex.ru>
2022-06-07 15:35:11 +0300

    logger added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 const { loginVal, createUserVal } = require('./errors/validation');
 const { errorHandler } = require('./errors/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -23,11 +24,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.post('/signin', loginVal, login);
 app.post('/signup', createUserVal, createUser);
 
 app.use('/', auth, require('./routes/users'));
 app.use('/', auth, require('./routes/cards'));
+
+app.use(errorLogger);
 
 app.use((req, res, next) => {
   res.status(404).send({ message: 'Не найдено' });
