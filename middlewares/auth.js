7fbd2dd94dec6d1556582dc4583d6984659b24f7
Viktoria Vishniakova <vikaveter1563@yandex.ru>
2022-06-11 02:12:15 +0300

    fixed auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,12 @@ const jwt = require('jsonwebtoken');
 const BadLoginErr = require('../errors/badLoginErr');
 
 module.exports.auth = (req, res, next) => {
-  const token = req.cookies.jwt || req.headers.authorization.replace('Bearer ', '');
+  let token = null;
+  if (req.headers.authorization) {
+    token = req.headers.authorization.replace('Bearer ', '');
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
   let payload;
   try {
     payload = jwt.verify(token, 'secret');
