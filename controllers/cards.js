ab4abef072a0ecb303bf97736a8ab193d14681bc
Viktoria Vishniakova <vikaveter1563@yandex.ru>
2022-06-12 01:45:56 +0300

    added populate owner in card creation

--- controllers/cards.js
+++ controllers/cards.js
@@ -14,6 +14,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
+    .populate('owner')
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
