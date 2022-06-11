5a51d7c684b32a7c77cc752c80251c0c388ce8ca
Viktoria Vishniakova <vikaveter1563@yandex.ru>
2022-06-11 19:56:29 +0300

    added debug log

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,8 @@ const BadLoginErr = require('../errors/badLoginErr');
 module.exports.auth = (req, res, next) => {
   let token = null;
   if (req.headers.authorization) {
+    console.log(req.headers);
+    console.log(req.headers.authorization);
     token = req.headers.authorization.replace('Bearer ', '');
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
